b7013bf6cee28f6d9934b03a8fa9e7ba4b737baf
Tushar040903 <tusharbhardwaj9873010398@gmail.com>
2024-07-22 17:57:00 +0530

    updating progress

---
+++ Day 8/Q4. Remove Duplicated from Sorted Linked List.cpp
@@ -0,0 +1,18 @@
+ListNode* deleteDuplicates(ListNode* head) {
+        if(head == NULL) return head;
+        if(head->next == NULL) return head;
+        ListNode* temp = head;
+
+        while(temp!=NULL) {
+            if(temp->next != NULL && temp->val == temp->next->val) {
+                ListNode* nextNode = temp->next;
+                temp->next = nextNode->next;
+                nextNode->next = NULL;
+                delete nextNode;
+            }
+            else {
+                temp = temp->next;
+            }
+        }
+        return head;
+    }
