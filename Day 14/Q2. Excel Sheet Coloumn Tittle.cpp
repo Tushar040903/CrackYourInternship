0c2645eff4e31a26498bbaf1d6f7dd9d07b7794e
Tushar040903 <tusharbhardwaj9873010398@gmail.com>
2024-07-28 19:39:31 +0530

    upadating progress

---
+++ Day 14/Q2. Excel Sheet Coloumn Tittle.cpp
@@ -0,0 +1,11 @@
+string convertToTitle(int n) {
+        string ans = "";
+        while(n>0){
+            n--;
+            int currentdigit = n%26;
+            n = n/26;
+            ans.push_back(currentdigit+'A');
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;        
+    }
